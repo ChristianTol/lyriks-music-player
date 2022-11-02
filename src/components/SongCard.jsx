d41df281c4273d91aaf4cfd786ed96c62235037b
ChristianTol <christian.tol1998@hotmail.com>
2022-11-02 11:10:35 +0100

    PlayPause functionality added.

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,8 +4,17 @@ import { useDispatch } from 'react-redux';
 import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
-const SongCard = ({ song, i }) => {
-  const activeSong = 'Test';
+const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
+  const dispatch = useDispatch();
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5
@@ -17,7 +26,13 @@ const SongCard = ({ song, i }) => {
         items-center bg-black bg-opacity-50 
         group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'} `}
         >
-          <PlayPause />
+          <PlayPause
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+          />
         </div>
         <img src={song.images?.coverart} alt="song_img" />
       </div>
