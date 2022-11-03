5a03d9ab766242d8952af6158c3a5158aa8df2cc
ChristianTol <christian.tol1998@hotmail.com>
2022-11-03 13:09:01 +0100

    Around You Country list added.

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,25 +1,14 @@
 import { useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
-import { setActiveSong, playPause } from '../redux/features/playerSlice';
 import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
 
 const ArtistDetails = () => {
-  const dispatch = useDispatch();
   const { id: artistId } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
 
   const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery(artistId);
-
-  const handlePauseClick = () => {
-    dispatch(playPause(false));
-  };
-
-  const handlePlayClick = (song, i) => {
-    dispatch(setActiveSong({ song, artistData, i }));
-    dispatch(playPause(true));
-  };
 
   if (isFetchingArtistDetails) return <Loader title="Loading artist details" />;
 
@@ -34,8 +23,6 @@ const ArtistDetails = () => {
         artistId={artistId}
         isPlaying={isPlaying}
         activeSong={activeSong}
-        handlePauseClick={handlePauseClick}
-        handlePlayClick={handlePlayClick}
       />
     </div>
   );
